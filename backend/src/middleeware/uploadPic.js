28469bae40b1c7498521022baa6b7928f8bb2110
mochafrizal <mochafrizalm60@gmail.com>
2025-01-02 16:35:01 +0700

    vercel

--- backend/src/middleeware/uploadPic.js
+++ backend/src/middleeware/uploadPic.js
@@ -8,20 +8,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        const uploadsPath = path.join(__dirname, '../uploads'); 
-        
+    destination: (req, file, cb) => {
+        const uploadsPath = path.join(__dirname, '../uploads');
+
         if (!fs.existsSync(uploadsPath)) {
-            fs.mkdirSync(uploadsPath); 
+            fs.mkdirSync(uploadsPath);
         }
-        
-        cb(null, uploadsPath); 
+
+        cb(null, uploadsPath);
     },
     filename: (req, file, cb) => {
         const unique = Date.now() + Math.round(Math.random() * 1E9);
-    
+
         const ext = path.extname(file.originalname);
-        
+
         cb(null, file.fieldname + '-' + unique + ext);
     }
 });
